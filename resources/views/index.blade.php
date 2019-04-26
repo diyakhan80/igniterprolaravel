c76f15ba2b9c0ef17ff88d54216fb36dcc0a8bea
preetiverma123 <preetiverma123>
2019-04-26 16:54:23 +0530

    chanes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -290,7 +290,176 @@
      </div>
  </section>
  
+ <!-- [our build services]
+ ============================================================================================================================-->
+<section class="bg-white webservicesection">
+  <div class="container">
+    <div class="solutions_sec" style="text-align: center;">
+      <h3 class="title">Our <span class="themecolor">Web Services</span></h3>
+
+      <div class="sol_sec_list">
+        <div class="solution_list">
+          <ul>
+            <li>
+              <div class="solution_area rel">
+                <div class="hover_box2 rel">
+                  <!-- <p class="arrow_pos_home"><span class="fa fa-sort-asc arrow_col fs40"></span></p> -->
+                <div>
+                <p class="hover_box_h">Web Designing</p>
+                <div class="web_d_box">
+                  <div class="list_web">
+                    <ul>
+                      <li><a href="javascript:void(0);" title="Custom Website Design">Custom Website Design</a></li>
+                      <li><a href="javascript:void(0);" title="Responsive Web Design">Responsive Web Design</a></li>
+                      <li><a href="javascript:void(0);" title="Logo Design">Logo Design</a></li>
+                      <li><a href="javascript:void(0);" title="Ecommerce Web Design">Ecommerce Web Design</a></li>
+                    </ul>
+                  </div>
+                  <div class="view_pos">
+                  <p class="roboto fs13 weight700 white text-center"><a href="javascript:void(0);">View <br>
+                  All</a></p>
+                  </div>
+                </div>
+                </div>
+                </div>
+              <div class="solution_img"><figure><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="web-design" x="0" y="0" width="76" height="70" viewBox="0 0 76 70" xml:space="preserve" enable-background="new 0 0 76 70"><path d="M64.68 5.18H13.32c-5.14 0-10.27 5.14-10.27 10.27v41.1c0 5.14 5.14 10.28 10.27 10.28h51.37c5.14 0 10.27-5.14 10.27-10.27V15.45C74.96 10.31 69.82 5.18 64.68 5.18zM13.32 61.68H8.18v-5.14h5.14V61.68zM23.59 61.68h-5.14v-5.14h5.14V61.68zM69.82 61.68h-5.14v-5.14h5.14V61.68zM69.82 51.41H8.18V10.31H69.82V51.41zM21.02 25.56c1.31 0 2.37-1.06 2.37-2.37 0-1.31-1.06-2.37-2.37-2.37s-2.37 1.06-2.37 2.37C18.65 24.5 19.71 25.56 21.02 25.56zM15.96 46.27h48.73V16.64c-1.28-0.26-1.02 0.4-2.37 0.4-6.13 0-10.81 2.79-14.44 7.17-0.66-2.31-2.79-4.01-5.32-4.01-3.06 0-5.53 2.48-5.53 5.53l0.06 0.79c-5.86 0-10.85 3.99-12.28 9.4-1.21-0.94-2.72-1.5-4.37-1.5-3.93 0-7.11 2.4-7.11 6.32C13.32 42.97 14.34 44.97 15.96 46.27z" style="clip-rule:evenodd;fill-rule:evenodd;fill:#3DB4E1"></path></svg>
+              </figure></div>
+              <p class="text-center mt10"><span class="fa fa-sort-asc arrow_col fs40"></span></p>
+              <div class="solution_h_txt">Web design</div>
+              </div>
+            </li>
+            <li>
+            <div class="solution_area rel">
+            <div class="hover_box2 rel">
+            <!-- <p class="arrow_pos_home"><span class="fa fa-sort-asc arrow_col fs40"></span></p> -->
+            <div>
+            <p class="hover_box_h">Web Development</p>
+            <div class="web_d_box">
+            <div class="list_web">
+              <ul>
+                <li>
+                  <a href="javascript:void(0);" title="Joomla Web Development">Joomla Web Development</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);" title="PHP Web Development">PHP Web Development</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);" title="CMS Web Development">CMS Web Development</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);" title="">WordPress Web Development</a>
+                </li>
+              </ul>
+            </div>
+            <div class="view_pos">
+            <p class="roboto fs13 weight700 white text-center"><a href="javascript:void(0);">View <br>
+            All</a></p>
+            </div>
+            </div>
+            </div>
+            </div>
+
+
+            <div class="solution_img"><figure><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="web-development" x="0" y="0" width="77" height="76" viewBox="0 0 77 76" xml:space="preserve" enable-background="new 0 0 77 76"><path d="M43.09 45.43c0.35 0.22 0.76 0.34 1.18 0.34 0.32 0 0.63-0.07 0.92-0.2l11.24-5.23c0.77-0.36 1.26-1.14 1.26-1.98v-0.16c0-0.85-0.5-1.62-1.26-1.98l-11.24-5.23c-0.28-0.13-0.6-0.2-0.92-0.2 -0.42 0-0.83 0.12-1.18 0.34 -0.63 0.4-1.01 1.09-1.01 1.84v0.06c0 0.85 0.5 1.62 1.26 1.98l7.03 3.27 -7.03 3.27c-0.77 0.36-1.26 1.14-1.26 1.98v0.06C42.08 44.34 42.46 45.03 43.09 45.43zM51.95 64.38c-0.39-0.23-0.73-0.55-0.99-0.94 -0.12-0.18-0.23-0.36-0.31-0.56H6.58V19.45h59.8v28.36c1.28 0.11 2.35 0.95 2.8 2.1 0.4-0.22 0.84-0.35 1.31-0.39 0.31-0.03 0.63-0.01 0.93 0.05V8.63c0-2.31-1.88-4.2-4.2-4.2H5.74c-2.31 0-4.2 1.89-4.2 4.2v55.1c0 2.32 1.88 4.2 4.2 4.2h45.83c-0.36-0.73-0.44-1.54-0.23-2.33C51.46 65.15 51.67 64.74 51.95 64.38zM61.77 11.48c0-0.85 0.69-1.55 1.55-1.55h1.55c0.86 0 1.55 0.69 1.55 1.55v1.76c0 0.86-0.69 1.55-1.55 1.55h-1.55c-0.86 0-1.55-0.69-1.55-1.55V11.48zM54.56 11.48c0-0.85 0.69-1.55 1.55-1.55h1.55c0.86 0 1.55 0.69 1.55 1.55v1.76c0 0.86-0.69 1.55-1.55 1.55h-1.55c-0.86 0-1.55-0.69-1.55-1.55V11.48zM47.35 11.48c0-0.85 0.7-1.55 1.55-1.55h1.55c0.86 0 1.55 0.69 1.55 1.55v1.76c0 0.86-0.69 1.55-1.55 1.55h-1.55c-0.85 0-1.55-0.69-1.55-1.55V11.48zM6.19 11.48c0-0.85 0.69-1.55 1.55-1.55h30.06c0.86 0 1.55 0.69 1.55 1.55v1.76c0 0.86-0.69 1.55-1.55 1.55H7.74c-0.86 0-1.55-0.69-1.55-1.55V11.48zM32.75 51.89h0.06c0.95 0 1.79-0.61 2.08-1.52l7.4-22.95c0.21-0.66 0.1-1.39-0.31-1.95 -0.41-0.56-1.07-0.9-1.76-0.9h-0.06c-0.95 0-1.79 0.61-2.08 1.52l-7.4 22.95c-0.21 0.66-0.09 1.39 0.32 1.95C31.4 51.55 32.06 51.89 32.75 51.89zM30.89 33.03v-0.06c0-0.75-0.38-1.44-1.01-1.84 -0.35-0.22-0.76-0.34-1.18-0.34 -0.32 0-0.63 0.07-0.92 0.2l-11.24 5.23c-0.77 0.36-1.26 1.14-1.26 1.98v0.16c0 0.85 0.5 1.62 1.26 1.98l11.24 5.23c0.29 0.13 0.6 0.2 0.92 0.2 0.42 0 0.82-0.12 1.18-0.34 0.63-0.4 1.01-1.09 1.01-1.84V43.53c0-0.85-0.5-1.62-1.26-1.98l-7.03-3.27 7.03-3.27C30.39 34.66 30.89 33.88 30.89 33.03zM75.89 58.78c-0.06-0.36-0.4-0.59-0.76-0.53l-2.35 0.42c-0.28-0.66-0.64-1.28-1.07-1.85l1.54-1.83c0.11-0.13 0.17-0.3 0.15-0.48 -0.02-0.17-0.1-0.33-0.23-0.44l-2.06-1.73c-0.13-0.11-0.3-0.16-0.48-0.15 -0.17 0.02-0.33 0.1-0.44 0.23l-1.53 1.83c-0.63-0.32-1.3-0.57-2.01-0.73v-2.39c0-0.36-0.29-0.65-0.65-0.65H63.31c-0.36 0-0.65 0.29-0.65 0.65v2.39c-0.71 0.16-1.38 0.41-2.01 0.73l-1.53-1.83c-0.23-0.28-0.65-0.31-0.92-0.08l-2.06 1.73c-0.13 0.11-0.22 0.27-0.23 0.44 -0.02 0.17 0.04 0.35 0.15 0.48l1.54 1.83c-0.43 0.57-0.79 1.19-1.07 1.85l-2.35-0.41c-0.35-0.06-0.69 0.18-0.76 0.53l-0.47 2.64c-0.03 0.17 0.01 0.35 0.11 0.49 0.1 0.14 0.25 0.24 0.42 0.27l2.35 0.41c0.04 0.73 0.17 1.44 0.37 2.11l-2.07 1.19c-0.15 0.09-0.26 0.23-0.3 0.4s-0.02 0.35 0.07 0.5l1.34 2.32c0.09 0.15 0.23 0.26 0.4 0.31 0.06 0.02 0.11 0.02 0.17 0.02 0.11 0 0.23-0.03 0.33-0.09l2.07-1.19c0.49 0.52 1.04 0.98 1.64 1.37l-0.82 2.24c-0.06 0.16-0.05 0.34 0.02 0.5 0.07 0.16 0.21 0.28 0.37 0.34l2.52 0.92c0.07 0.03 0.15 0.04 0.22 0.04 0.1 0 0.19-0.02 0.28-0.06 0.16-0.07 0.28-0.21 0.34-0.37l0.82-2.24c0.35 0.04 0.71 0.06 1.07 0.06s0.72-0.02 1.07-0.06l0.82 2.24c0.06 0.16 0.18 0.3 0.34 0.37 0.09 0.04 0.18 0.06 0.28 0.06 0.08 0 0.15-0.01 0.22-0.04l2.52-0.92c0.16-0.06 0.3-0.18 0.37-0.34 0.07-0.16 0.08-0.34 0.02-0.5l-0.82-2.24c0.6-0.39 1.15-0.85 1.64-1.37l2.07 1.19c0.1 0.06 0.21 0.09 0.33 0.09 0.06 0 0.11-0.01 0.17-0.02 0.17-0.05 0.31-0.15 0.4-0.31l1.34-2.32c0.09-0.15 0.11-0.33 0.07-0.5s-0.15-0.31-0.3-0.4l-2.07-1.19c0.2-0.67 0.33-1.38 0.37-2.1l2.35-0.41c0.17-0.03 0.32-0.13 0.42-0.27 0.1-0.14 0.14-0.32 0.11-0.49L75.89 58.78zM64.65 65.62c-1.99 0-3.6-1.61-3.6-3.6 0-1.99 1.61-3.6 3.6-3.6s3.6 1.61 3.6 3.6C68.25 64.01 66.64 65.62 64.65 65.62z" style="clip-rule:evenodd;fill-rule:evenodd;fill:#FF8515"></path></svg>
+            </figure></div>
+            <p class="text-center mt10"><span class="fa fa-sort-asc arrow_col fs40"></span></p>
+            <div class="solution_h_txt">Web Development</div>
+            </div>
+            </li>
+            <li>
+            <div class="solution_area rel">
+
+            <div class="hover_box2 rel">
+            <!-- <p class="arrow_pos_home"><span class="fa fa-sort-asc arrow_col fs40"></span></p> -->
+            <div>
+            <p class="hover_box_h">Digital Marketing</p>
+            <div class="web_d_box">
+            <div class="list_web">
+              <ul>
+                <li>
+                  <a href="javascript:void(0);javascript:void(0);" title="Search Engine Optimization">Search Engine Optimization</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);javascript:void(0);" title="Affiliate Marketing">Affiliate Marketing</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);javascript:void(0);" title="Content Writing">Content Writing</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);javascript:void(0);" title="Online Marketing">Online Marketing</a>
+                </li> 
+              </ul>
+            </div>
+            <div class="view_pos">
+            <p class="roboto fs13 weight700 white text-center"><a href="javascript:void(0);">View <br>
+            All</a></p>
+            </div>
+            </div>
+            </div>
+            </div>
+
+            <div class="solution_img"><figure><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="digital-marketing" x="0" y="0" width="77" height="76" viewBox="0 0 77 76" xml:space="preserve" enable-background="new 0 0 77 76"><path d="M37.19 14.64c1.9 0.66 3.43 2.03 4.3 3.84 0.56 1.15 0.77 2.39 0.71 3.63l2.34 4.86c0.09-0.03 0.18-0.03 0.27-0.07 0.5-0.24 0.91-0.67 1.11-1.24 1.05-2.99 0.87-6.21-0.51-9.07 -1.38-2.85-3.78-5-6.78-6.05 -2.99-1.04-6.21-0.86-9.07 0.52 -2.86 1.38-5.01 3.79-6.05 6.78 -1.05 2.99-0.87 6.21 0.51 9.07 1.32 2.74 3.6 4.8 6.42 5.88 0.26-0.17 0.5-0.37 0.78-0.5 0.23-0.11 0.47-0.21 0.71-0.3l-2.22-4.6c-0.74-0.65-1.35-1.45-1.79-2.37 -0.87-1.81-0.99-3.85-0.32-5.75C28.92 15.48 33.39 13.32 37.19 14.64zM25.33 37.99c0.69-0.33 1.4-0.54 2.12-0.62 0.05-0.34 0.17-0.66 0.27-0.99 -3.34-1.58-6.07-4.17-7.69-7.55 -1.89-3.91-2.14-8.33-0.7-12.43C22.3 7.93 31.6 3.44 40.07 6.39c4.11 1.43 7.4 4.38 9.29 8.29 1.89 3.91 2.14 8.33 0.7 12.43 -0.33 0.94 0.06 1.93 0.84 2.47 0.2-0.12 0.38-0.27 0.59-0.37 0.82-0.4 1.72-0.55 2.62-0.6 0.01-0.02 0.03-0.04 0.04-0.07 1.82-5.2 1.51-10.79-0.89-15.74S46.7 4.11 41.5 2.3C30.77-1.44 18.99 4.24 15.24 14.97c-1.82 5.2-1.5 10.79 0.89 15.75 1.7 3.52 4.31 6.41 7.54 8.44C24.15 38.7 24.7 38.29 25.33 37.99zM54.97 31.46c-0.79-0.11-1.55 0.03-2.22 0.35 -1.13 0.54-1.97 1.62-2.16 2.96l-1.08 7.73c-0.02 0.12-0.09 0.21-0.19 0.26 -0.03 0.02-0.06 0.03-0.1 0.03 -0.15 0.02-0.3-0.05-0.36-0.19L37.81 19.68c-0.93-1.93-3.25-2.73-5.18-1.81 -1.93 0.93-2.74 3.25-1.81 5.18l6.1 12.65c-1.13-1.16-2.91-1.54-4.45-0.8 -1.93 0.93-2.74 3.25-1.81 5.17l1.12 2.33c-0.93-1.93-3.25-2.74-5.18-1.8 -1.93 0.93-2.74 3.25-1.81 5.18l0.66 1.38c-0.43 0.05-0.86 0.15-1.27 0.35 -1.93 0.93-2.74 3.25-1.81 5.18l10.57 21.91c0.37 0.76 1.29 1.08 2.05 0.71l18.27-8.81c0.46-0.22 0.78-0.66 0.85-1.17l4.17-29.48C58.57 33.72 57.09 31.76 54.97 31.46z" style="clip-rule:evenodd;fill-rule:evenodd;fill:#0CC5CC"></path></svg>
+            </figure></div>
+            <p class="text-center mt10"><span class="fa fa-sort-asc arrow_col fs40"></span></p>
+            <div class="solution_h_txt">digital marketing</div>
+            </div>
+            </li>
+            <li>
+            <div class="solution_area rel">
+
+            <div class="hover_box2 rel">
+            <!-- <p class="arrow_pos_home"><span class="fa fa-sort-asc arrow_col fs40"></span></p> -->
+            <div>
+            <p class="hover_box_h">apps Development</p>
+            <div class="web_d_box">
+            <div class="list_web">
+              <ul>
+                <li>
+                  <a href="javascript:void(0);" title="Iphone App Development">IOS App Development</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);" title="Android App Development ">Android App Development </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);" title="Blackberry App Development">Blackberry App Development</a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);" title="Windows App Development">Windows App Development</a>
+                </li>
+              </ul>
+            </div>
+            <div class="view_pos">
+            <p class="roboto fs13 weight700 white text-center"><a href="javascript:void(0);">View <br>
+            All</a></p>
+            </div>
+            </div>
+            </div>
+            </div>
+
+            <div class="solution_img"><figure><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="apps-development" x="0" y="0" width="56" height="82" viewBox="0 0 56 82" xml:space="preserve" enable-background="new 0 0 56 82"><path d="M16.01 36.67h1.26c0.21 0.74 0.5 1.44 0.87 2.1l-1.16 1.17c-0.74 0.75-0.74 1.95 0 2.7l0.68 0.68c0.75 0.75 1.95 0.75 2.7 0l1.17-1.17c0.65 0.37 1.35 0.66 2.09 0.86v1.22c0 1.05 0.85 1.91 1.91 1.91h0.95c1.05 0 1.91-0.85 1.91-1.91v-1.22c0.74-0.2 1.44-0.49 2.09-0.86l1.17 1.17c0.75 0.75 1.95 0.75 2.7 0l0.68-0.67c0.75-0.75 0.75-1.95 0-2.7l-1.16-1.16c0.38-0.65 0.67-1.35 0.88-2.1h1.26c1.05 0 1.91-0.85 1.91-1.91v-0.95c0-1.05-0.85-1.91-1.91-1.91h-1.24c-0.2-0.73-0.48-1.43-0.84-2.08l1.12-1.12c0.75-0.74 0.75-1.95 0-2.7l-0.67-0.68c-0.74-0.74-1.95-0.74-2.7 0l-1.1 1.1c-0.67-0.39-1.4-0.69-2.16-0.9v-1.28c0-1.05-0.85-1.91-1.91-1.91h-0.95c-1.05 0-1.91 0.85-1.91 1.91v1.28c-0.77 0.21-1.49 0.51-2.16 0.9l-1.1-1.1c-0.74-0.74-1.95-0.74-2.7 0L16.98 26c-0.74 0.74-0.74 1.95 0 2.7l1.12 1.12c-0.36 0.65-0.65 1.35-0.84 2.08h-1.24c-1.05 0-1.91 0.85-1.91 1.91v0.95C14.1 35.81 14.95 36.67 16.01 36.67zM26 29.07c2.9 0 5.25 2.35 5.25 5.25 0 2.9-2.35 5.25-5.25 5.25 -2.9 0-5.25-2.35-5.25-5.25C20.75 31.41 23.1 29.07 26 29.07zM46.24 6.91H10.9c-3.25 0-5.89 2.64-5.89 5.89v58.9c0 3.25 2.64 5.89 5.89 5.89h35.34c3.25 0 5.89-2.64 5.89-5.89v-58.9C52.13 9.55 49.49 6.91 46.24 6.91zM19.74 9.85h14.72l0.19 2.94H19.74V9.85zM28.51 73.17c-1.63 0-2.94-1.32-2.94-2.94 0-1.62 1.32-2.94 2.95-2.94 1.63 0 2.95 1.32 2.95 2.94C31.46 71.85 30.14 73.17 28.51 73.17zM46.24 62.86H10.9l0.06-47.12h35.28V62.86zM26 36.64c1.32 0 2.38-1.07 2.38-2.38s-1.07-2.38-2.38-2.38c-1.32 0-2.38 1.07-2.38 2.38S24.68 36.64 26 36.64zM37.74 45.17c0-0.6-0.49-1.09-1.09-1.09h-0.54c-0.6 0-1.09 0.49-1.09 1.09v0.7c-0.42 0.12-0.82 0.28-1.19 0.49l-0.67-0.67c-0.42-0.43-1.11-0.43-1.54 0l-0.38 0.38c-0.42 0.43-0.42 1.12 0 1.54l0.66 0.66c-0.21 0.37-0.38 0.77-0.5 1.2h-0.72c-0.6 0-1.09 0.49-1.09 1.09v0.55c0 0.6 0.49 1.09 1.09 1.09h0.71c0.11 0.42 0.27 0.82 0.48 1.19l-0.64 0.64c-0.42 0.43-0.42 1.11 0 1.54l0.39 0.39c0.43 0.43 1.12 0.43 1.54 0l0.63-0.63c0.38 0.22 0.8 0.4 1.23 0.52v0.73c0 0.6 0.49 1.09 1.09 1.09h0.54c0.6 0 1.09-0.49 1.09-1.09v-0.73c0.44-0.12 0.85-0.29 1.23-0.52l0.63 0.63c0.43 0.43 1.12 0.43 1.54 0l0.39-0.38c0.43-0.43 0.43-1.11 0-1.54l-0.64-0.64c0.21-0.37 0.37-0.77 0.48-1.19h0.71c0.6 0 1.09-0.49 1.09-1.09v-0.55c0-0.6-0.49-1.09-1.09-1.09h-0.72c-0.12-0.42-0.28-0.82-0.5-1.2l0.66-0.66c0.43-0.42 0.43-1.11 0-1.54l-0.38-0.38c-0.42-0.43-1.11-0.43-1.54 0l-0.67 0.67c-0.37-0.21-0.77-0.37-1.19-0.49V45.17zM39.37 50.81c0 1.65-1.34 2.99-2.99 2.99 -1.65 0-2.99-1.34-2.99-2.99s1.34-2.99 2.99-2.99C38.03 47.82 39.37 49.16 39.37 50.81zM35.02 50.85c0 0.75 0.61 1.36 1.36 1.36 0.75 0 1.36-0.61 1.36-1.36 0-0.75-0.61-1.36-1.36-1.36C35.63 49.49 35.02 50.1 35.02 50.85z" style="clip-rule:evenodd;fill-rule:evenodd;fill:#00C6A3"></path></svg>
+            </figure></div>
+            <p class="text-center mt10"><span class="fa fa-sort-asc arrow_col fs40"></span></p>
+            <div class="solution_h_txt">apps Development</div>
+            </div>
+            </li>
+          </ul>
+        <div class="clearfix"></div>
+        </div>
+        <div class="clearfix"></div>
+        </div>
+    </div>
+  </div>
+</section>
  
+<!-- [our build services]
+ ============================================================================================================================-->
+ <section class="technologysection">
+   <div class="container">
+     <div class="row">
+       
+     </div>
+   </div>
+ </section>
  <!-- [/OUR VISION]
  ============================================================================================================================-->
  <section class="aboutus white-background black section-gap section-gradiant" id="one">
@@ -415,7 +584,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-              <h3 class="title"><span class="themecolor">Recent Works</span></h3>
+              <h3 class="title">Recent <span class="themecolor">Works</span></h3>
             <p class="a-slog text-center">These  are some of the work projects that we recently developed ...</p>
           </div> 
         </div>
