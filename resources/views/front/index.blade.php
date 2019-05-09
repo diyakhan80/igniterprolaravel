7dbfe649db3b63d91848c78fd32be680091ea135
unknown <preetiverma16lko@gmail.com>
2019-05-09 10:46:05 +0530

    changes

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -968,12 +968,6 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-        <h3 class="enquiryhead">Make An Enquiry</h3>
-        <div class="enquiryTop">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-      </div>
-      <div class="col-md-6">
         <div class="btnregister">
           <a class="demoregisterbtn" href="{{url('registration')}}">Register for demo classes</a>
         </div>
@@ -1016,14 +1010,13 @@
             </form>        
         </div>
       </div>
+      <div class="col-md-6">
+        <div class="mapsection">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3345.4913678080866!2d81.01918631488186!3d26.85528298315246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399be2f0348b3347%3A0x513c8138a275f155!2s3%2F5%2C+Vastu+Khand+Road%2C+Virat+Khand+3%2C+Gomti+Nagar%2C+Lucknow%2C+Uttar+Pradesh+226010!5e1!3m2!1sen!2sin!4v1556518773562!5m2!1sen!2sin" width="100%" height="625" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </section>
 
-<section>
-  <div class="container">
-    <div class="mapsection">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3345.4913678080866!2d81.01918631488186!3d26.85528298315246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399be2f0348b3347%3A0x513c8138a275f155!2s3%2F5%2C+Vastu+Khand+Road%2C+Virat+Khand+3%2C+Gomti+Nagar%2C+Lucknow%2C+Uttar+Pradesh+226010!5e1!3m2!1sen!2sin!4v1556518773562!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div>
-  </div>
-</section>
